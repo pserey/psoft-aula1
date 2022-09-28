e4f9a2a0fa732a5c9dd471e24a15040a0de685e1
Pedro Serey <serey.pedro@gmail.com>
2022-09-28 11:32:07 -0300

    commit inicial

---
+++ src/Produto.java
@@ -0,0 +1,36 @@
+public class Produto {
+    String nome;
+    String fabricante;
+    double preco;
+
+    public Produto(String nome, String fabricante, double preco) {
+        this.nome = nome;
+        this.fabricante = fabricante;
+        this.preco = preco;
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getFabricante() {
+        return this.fabricante;
+    }
+
+    public void setFabricante(String fabricante) {
+        this.fabricante = fabricante;
+    }
+
+    public double getPreco() {
+        return this.preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+}
+
---
+++ src/Sistema.java
@@ -0,0 +1,5 @@
+public class Sistema {
+    public static void main(String[] args) {
+        Produto cadeira = new Produto("Cadeira", "Cadeiras LTDA", "25")
+    }
+}
